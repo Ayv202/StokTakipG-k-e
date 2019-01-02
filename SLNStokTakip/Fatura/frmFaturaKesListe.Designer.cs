dbe3e9b7d0620559ecdf69f9079a4cd65bb1d9c3
gokceaslan2112 <gaslan2112@gmail.com>
2019-01-02 16:29:49 +0300

    Add project files.

---
+++ SLNStokTakip/Fatura/frmFaturaKesListe.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace SLNStokTakip.Fatura
+{
+    partial class frmFaturaKesListe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmFaturaKesListe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmFaturaKesListe";
+            this.Text = "frmFaturaKesListe";
+            this.Load += new System.EventHandler(this.frmFaturaKesListe_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ SLNStokTakip/Fatura/frmFaturaKesListe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SLNStokTakip.Fatura
+{
+    public partial class frmFaturaKesListe : Form
+    {
+        public bool Secim = false;
+
+        public frmFaturaKesListe()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFaturaKesListe_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
